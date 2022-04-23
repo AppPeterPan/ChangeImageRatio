16ba754dbd5097a1c2914431d8ba7e3f3935230e
peterpan <apppeterpan@gmail.com>
2022-04-23 20:49:55 +0800

    update

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageRatioConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func changeRatio(_ sender: UISegmentedControl) {
+        imageRatioConstraint.isActive = false
+        if sender.selectedSegmentIndex == 0 {
+            imageRatioConstraint = imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1)
+        } else {
+            imageRatioConstraint = imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 16 / 9)
+        }
+        imageRatioConstraint.isActive = true
 
+    }
+    
 }
 
